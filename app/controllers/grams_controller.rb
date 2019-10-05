0a24770f55c9f823b1057f8a11e1efc7728fdd97
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-10-05 15:54:14 +0000

    Lesson 16

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -3,6 +3,7 @@ class GramsController < ApplicationController
 
 
   def index
+    @grams = Gram.all
   end
 
   def new
@@ -53,7 +54,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
